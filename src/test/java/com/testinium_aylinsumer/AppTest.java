ef0b60393fd477eff88e93653f5df3a0186ecf7a
aylinsumer <aylinsumer21@gmail.com>
2021-10-21 15:34:20 +0300

    testinium bağlantısı için chromedriver if/else döngüsüne alındı.

--- src/test/java/com/testinium_aylinsumer/AppTest.java
+++ src/test/java/com/testinium_aylinsumer/AppTest.java
@@ -1,27 +1,60 @@
 package com.testinium_aylinsumer;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Set;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class AppTest extends TestBase {
+public class AppTest {
+    public static WebDriver webDriver;
+    @BeforeClass
+    public static void setUp() {
+        try {
+            DesiredCapabilities capabilities =new DesiredCapabilities("chrome","95.0.4638.54", Platform.WIN10);
+            if (System.getProperty("key") != null) {
+                capabilities.setCapability("key", System.getProperty("key"));
+                webDriver = new RemoteWebDriver(new URL("http://hub.testinium.io/wd/hub"), capabilities);
+            }
+            else {
+                System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+                webDriver = new ChromeDriver();
+                webDriver.get("https://www.trendyol.com/");
+                webDriver.manage().window().maximize();
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        webDriver.findElement(By.id("Group-38")).click();
+    }
+    @Before
+    public void before(){
+        String currentWindow = webDriver.getWindowHandle();
+        Set<String> windowHandles = webDriver.getWindowHandles();
+        for (String window : windowHandles) {
+            if (!currentWindow.equals(window)) {
+                webDriver.switchTo().window(window);
+            }
+        }
+    }
 
     @Test
     public void a_loginTest() throws InterruptedException {
         webDriver.findElement(By.cssSelector("div.account-nav-item.user-login-container")).click();
         webDriver.findElement(By.id("login-email")).sendKeys("aylin.sumer@hotmail.com");
         webDriver.findElement(By.id("login-password-input")).sendKeys("aylinmorea1"+ Keys.ENTER);
-        Thread.sleep(3000);
+        Thread.sleep(5000);
     }
     @Test
     public void b_womanSectionTest(){
@@ -31,24 +64,24 @@ public class AppTest extends TestBase {
     @Test
     public void c_searchButtonTest() throws InterruptedException {
         webDriver.findElement(By.cssSelector("input[type=text]")).sendKeys("kazak"+Keys.ENTER);
-        Thread.sleep(5000);
+        Thread.sleep(3000);
     }
 
     @Test
     public void d_productSelectTest() throws InterruptedException {
-        Actions actions=new Actions(webDriver);
-        actions.moveToElement(webDriver.findElement(By.cssSelector("body"))).contextClick();
-//        webDriver.findElement(By.xpath("//body")).click();
+        webDriver.findElement(By.xpath("//body")).click();
         Thread.sleep(3000);
+
         List<WebElement> products = webDriver.findElements(By.cssSelector("div>div.prdct-cntnr-wrppr>div"));
         products.get(9).click();
         Thread.sleep(3000);
+
+
     }
 
     @Test
     public void e_addFavoriteTest() throws InterruptedException {
-
-        webDriver.findElement(By.cssSelector(".fv>.i-heart")).click();
+        webDriver.findElement(By.cssSelector(".fv>i.i-heart")).click();
         Thread.sleep(3000);
     }
 
@@ -76,5 +109,10 @@ public class AppTest extends TestBase {
 
      */
 
+    @AfterClass
+    public static void afterClass(){
+        //webDriver.quit();
+        System.out.println("Test page is closed.");
+    }
 
 }
